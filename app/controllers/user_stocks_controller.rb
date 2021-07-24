186c10f16154a73cd0eeed685bd31790818328fb
thurpe <babatope.ifesanya@gmail.com>
2021-07-24 16:53:44 +0100

    Implemented lastname and firstname in model, view and controller

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -12,7 +12,7 @@ class UserStocksController < ApplicationController
     end
     def destroy
         stock = Stock.find(params[:id])
-        @user_stock = UserStock.where(user_id: current_user.id, stock_id: stock_id).first
+        @user_stock = UserStock.where(user_id: current_user.id, stock_id: stock.id).first
         @user_stock.destroy
         flash[:notice] = "Stock was successfully removed from your portfolio"
         redirect_to my_portfolio_path
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,15 @@ class User < ActiveRecord::Base
   has_many :user_stocks
   has_many :stocks, through: :user_stocks
 
+  def full_name
+    return "#{first_name} #{last_name}".strip if (first_name || last_name)
+    "Anonymous"
+  end
+
   def stock_already_added?(ticker_symbol)
       stock = Stock.find_by_ticker(ticker_symbol)
       return false unless stock
-      user_stock.where(stock_id: stock.id).exits?
+      user_stocks.where(stock_id: stock.id).exists?
   end
 
   def under_stock_limit?
