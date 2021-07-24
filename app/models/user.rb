d4852b701336c54cd7368fabcdb2c55404f710de
thurpe <babatope.ifesanya@gmail.com>
2021-07-24 17:15:58 +0100

    Implemented Friendship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
-class User < ActiveRecord::Base
+class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -6,6 +6,8 @@ class User < ActiveRecord::Base
 
   has_many :user_stocks
   has_many :stocks, through: :user_stocks
+  has_many :friendships
+  has_many :friends, through: :friendships
 
   def full_name
     return "#{first_name} #{last_name}".strip if (first_name || last_name)
