8317d81f4c56c68c88164f2b9a7a1cb54c1b5d49
thurpe <babatope.ifesanya@gmail.com>
2021-07-24 12:53:11 +0100

    Modified and refactored stock lookup functionality

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,6 +1,13 @@
 class StocksController < ApplicationController
     def search
-        @stock = Stock.new_from_lookup(params[:stock])
-        render 'users/my_portfolio'
+        if params[:stock].blank?
+            flash.now[:danger] = "Search field can't be blank"
+        else
+            @stock = Stock.new_from_lookup(params[:stock])
+            flash.now[:danger] = "Symbol is invalid" unless @stock
+        end
+        respond_to do |format|
+            format.js { render partial: 'users/result' }
+        end
     end
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,12 +1,13 @@
-class Stock < ApplicationRecord
+class Stock < ActiveRecord::Base
 
     def self.new_from_lookup(ticker_symbol)
-        looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
-        price = strip_commas(looked_up_stock.l)
-        new(name: looked_up_stock.name, ticker: looked_up_stock.symbol, last_price: price)
+        begin
+            StockQuote::Stock.new(api_key: 'pk_96cb823eb97349669920a7bfa27b9f13')
+            looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
+            new(name: looked_up_stock.company_name, ticker: looked_up_stock.symbol, last_price: looked_up_stock.latest_price)
+        rescue Exception => e
+            return nil
+        end
     end
 
-    def self.strip_commas(number)
-        number.gsub(',', '')
-    end
 end
